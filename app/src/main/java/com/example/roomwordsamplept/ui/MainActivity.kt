ed9f6963bd87b20c6f60b2a5816762c530769555
gmzucolo <gustavo.zucolo@hotmail.com>
2022-01-26 22:09:43 -0300

    create Word.kt, WordDao.kt

--- app/src/main/java/com/example/roomwordsamplept/ui/MainActivity.kt
+++ app/src/main/java/com/example/roomwordsamplept/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.roomwordsamplept
+package com.example.roomwordsamplept.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.roomwordsamplept.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
